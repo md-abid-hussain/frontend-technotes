3730486fb5f58f9895d6f2eb869d6b29f4a3bc6c
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-08-19 12:14:32 +0530

    created welcome page for dashboard

---
+++ src/Features/Auth/Welcome.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons"
+
+const Welcome = () => {
+
+    const date = new Date()
+    const today = new Intl.DateTimeFormat('en-US',{dateStyle:'full',timeStyle:'long'}).format(date)
+
+    const content = (
+        <main className="welcome">
+            <p>{today}</p>
+            <h1>Welcome!</h1>
+            <ul>
+                <li><Link to="/dash/notes"><FontAwesomeIcon icon={faArrowCircleRight}/> View TechNotes</Link></li>
+                <li><Link to="/dash/users"><FontAwesomeIcon icon={faArrowCircleRight}/> View User Settings</Link></li>
+            </ul>
+        </main>
+    )
+  return content
+}
+
+export default Welcome
