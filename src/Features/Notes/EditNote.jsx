60e23f023fa8b0595960d7078355bb0c4b402d3d
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-08-25 18:46:27 +0530

    created edit note page

--- src/Features/Notes/EditNote.jsx
+++ src/Features/Notes/EditNote.jsx
@@ -1,8 +1,23 @@
+import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectNoteById } from "./notesApiSlice"
+import { selectAllUsers } from "../Users/usersApiSlice"
+import EditNoteForm from './EditNoteForm'
 
 const EditNote = () => {
+  const {id} = useParams()
+
+  const note = useSelector(state=>selectNoteById(state,id))
+  const users = useSelector(selectAllUsers)
+
+  const content = note && users ? <EditNoteForm note={note} users={users}/> : <p>Loading...</p>
+
   return (
-    <div>EditNote</div>
+    <section className="grid center form_container">
+      {content}
+    </section>
   )
+
 }
 
 export default EditNote
