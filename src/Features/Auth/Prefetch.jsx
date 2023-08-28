bf3dc7e3aca6e78b207bc3f8949c4dcc9f792386
Md Abid Hussain <101964499+md-abid-hussain@users.noreply.github.com>
2023-08-28 10:36:49 +0530

    Update Prefetch.jsx
    fixed the typo

--- src/Features/Auth/Prefetch.jsx
+++ src/Features/Auth/Prefetch.jsx
@@ -1,4 +1,4 @@
-import { store } from '../../App/store'
+import { store } from '../../app/store'
 import { notesApiSlice } from '../Notes/notesApiSlice'
 import { usersApiSlice } from '../Users/usersApiSlice'
 import { useEffect } from 'react'
