42b2a1ce43fe76f1820ce66024346623e1057b58
MD ABID HUSSAIN <abid0hussain1733@gmail.com>
2023-09-09 20:17:59 +0530

    added PersistLogin, RequireAuth component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import EditUser from './Features/Users/EditUser'
 import NewNote from './Features/Notes/NewNote'
 import EditNote from './Features/Notes/EditNote'
 import Prefetch from './Features/Auth/Prefetch'
+import PersistLogin from './Features/Auth/PersistLogin'
+import RequireAuth from './Features/Auth/RequireAuth'
+import { ROLE } from './config/role'
 import {Route,Routes} from 'react-router-dom'
 
 function App() {
@@ -21,23 +24,29 @@ function App() {
         <Route index element={<Public/>}/>
 
         <Route path="login" element={<Login/>}/>
-        <Route element={<Prefetch/>}>
-          <Route path="dash" element={<DashLayout/>}>
+        <Route element={<PersistLogin/>}>
+          <Route element={<RequireAuth allowedRoles={[...Object.values(ROLE)]} />}>
+            <Route element={<Prefetch/>}>
+              <Route path="dash" element={<DashLayout/>}>
 
-            <Route index element={<Welcome/>}/>
-            
-            <Route path='users'>
-              <Route index element={<UsersList/>}/>
-              <Route path=':id' element={<EditUser/>}/>
-              <Route path='new' element={<NewUserForm/>}/>
-            </Route>
+                <Route index element={<Welcome/>}/>
+                
+                <Route element={<RequireAuth allowedRoles={[ROLE.Admin, ROLE.Manager]} />}>
+                  <Route path='users'>
+                    <Route index element={<UsersList/>}/>
+                    <Route path=':id' element={<EditUser/>}/>
+                    <Route path='new' element={<NewUserForm/>}/>
+                  </Route>
+                </Route>
 
-            <Route path='notes'>
-              <Route index element={<NotesList/>}/>
-              <Route path=':id' element={<EditNote/>}/>
-              <Route path='new' element={<NewNote/>}/>
+                <Route path='notes'>
+                  <Route index element={<NotesList/>}/>
+                  <Route path=':id' element={<EditNote/>}/>
+                  <Route path='new' element={<NewNote/>}/>
+                </Route>
+                
+              </Route>
             </Route>
-            
           </Route>
         </Route>
       </Route>
