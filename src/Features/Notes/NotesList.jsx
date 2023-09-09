644c20a2e96877dc9c710478e2fae3048ca3bde3
MD ABID HUSSAIN <abid0hussain1733@gmail.com>
2023-09-09 20:15:06 +0530

    completed NoteList component

--- src/Features/Notes/NotesList.jsx
+++ src/Features/Notes/NotesList.jsx
@@ -1,58 +1,79 @@
-import {useGetNotesQuery} from './notesApiSlice'
-import Note from './Note'
- 
+import { useGetNotesQuery } from "./notesApiSlice";
+import Note from "./Note";
+import useAuth from "../../hooks/useAuth";
+
 const NotesList = () => {
+  const { username, isManager, isAdmin } = useAuth();
 
   const {
-    data:notes,
+    data: notes,
     isLoading,
     isSuccess,
     isError,
-    error
-  } = useGetNotesQuery(undefined,{
-    pollingInterval:15000,
-    refetchOnFocus:true,
-    refetchOnMountOrArgChange:true
-  })
+    error,
+  } = useGetNotesQuery("notesList", {
+    pollingInterval: 15000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
 
-  let content ;
-  if(isLoading){
-    content = <p>Loading...</p>
+  let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
-  if(isError){
-    content = <p className='errMsg'>{error?.error}</p>
+  if (isError) {
+    console.log(error);
+    content = <p className="errmsg">{error?.data.message}</p>;
   }
 
-  if(isSuccess){
-    const {ids} = notes
+  if (isSuccess) {
+    const { ids, entities } = notes;
 
-    const tableContent = ids?.length
-          ? ids.map(noteId=><Note key={noteId} noteId={noteId}/>)
-          : null
+    let filteredIds;
+
+    if (isManager || isAdmin) {
+      filteredIds = [...ids];
+    } else {
+      filteredIds = ids.filter(
+        (nodeId) => entities[nodeId].username === username
+      );
+    }
+
+    const tableContent =
+      ids?.length &&
+      filteredIds.map((noteId) => <Note key={noteId} noteId={noteId} />);
 
     content = (
-      <table className='table table_notes'>
-        <thead className='table_head'>
+      <table className="table table_notes">
+        <thead className="table_head">
           <tr>
-            <th scope='col' className='table_th note_status'>Status</th>
-            <th scope='col' className='table_th note_created'>Created</th>
-            <th scope='col' className='table_th note_updated'>Updated</th>
-            <th scope='col' className='table_th note_title'>Title</th>
-            <th scope='col' className='table_th note_owner'>Owner</th>
-            <th scope='col' className='table_th note_edit'>Edit</th>
+            <th scope="col" className="table_th note_status">
+              Status
+            </th>
+            <th scope="col" className="table_th note_created">
+              Created
+            </th>
+            <th scope="col" className="table_th note_updated">
+              Updated
+            </th>
+            <th scope="col" className="table_th note_title">
+              Title
+            </th>
+            <th scope="col" className="table_th note_owner">
+              Owner
+            </th>
+            <th scope="col" className="table_th note_edit">
+              Edit
+            </th>
           </tr>
         </thead>
-        <tbody>
-          {tableContent}
-        </tbody>
+        <tbody>{tableContent}</tbody>
       </table>
-    )
+    );
   }
 
-  return <section>
-    {content}
-  </section>
-}
+  return <section>{content}</section>;
+};
 
-export default NotesList
+export default NotesList;
