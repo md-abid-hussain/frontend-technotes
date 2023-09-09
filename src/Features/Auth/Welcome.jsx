b4ce230fbf3c890cf9946cbcddf85c35cbeca472
MD ABID HUSSAIN <abid0hussain1733@gmail.com>
2023-09-09 20:15:54 +0530

    updated dashboard welcome page

--- src/Features/Auth/Welcome.jsx
+++ src/Features/Auth/Welcome.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons"
+import useAuth from "../../hooks/useAuth"
 
 const Welcome = () => {
+
+    const {username, isAdmin, isManager } = useAuth()
 
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-US',{dateStyle:'full',timeStyle:'long'}).format(date)
@@ -10,12 +13,16 @@ const Welcome = () => {
     const content = (
         <main className="welcome">
             <p>{today}</p>
-            <h1>Welcome!</h1>
+            <h1>Welcome! {username}</h1>
             <ul>
                 <li><Link to="/dash/notes"><FontAwesomeIcon icon={faArrowCircleRight}/> View TechNotes</Link></li>
                 <li><Link to="/dash/notes/new"><FontAwesomeIcon icon={faArrowCircleRight}/> Add new TechNotes</Link></li>
-                <li><Link to="/dash/users"><FontAwesomeIcon icon={faArrowCircleRight}/> View User Settings</Link></li>
-                <li><Link to="/dash/users/new"><FontAwesomeIcon icon={faArrowCircleRight}/> Add new User</Link></li>
+                {(isAdmin || isManager) && 
+                    <>
+                        <li><Link to="/dash/users"><FontAwesomeIcon icon={faArrowCircleRight}/> View User Settings</Link></li>
+                        <li><Link to="/dash/users/new"><FontAwesomeIcon icon={faArrowCircleRight}/> Add new User</Link></li>
+                    </>
+                }
             </ul>
         </main>
     )
