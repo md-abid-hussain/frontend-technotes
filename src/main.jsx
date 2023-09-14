d9fc173544c5657a7b672223578d392a5764c3ff
MD ABID HUSSAIN <abid0hussain1733@gmail.com>
2023-09-15 00:14:40 +0530

    disabled dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import './index.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import {store} from './app/store'
 import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (import.meta.env.PROD){
+  disableReactDevTools()
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
